acea42c7b9fdc5c853aae912c94a1017d1935314
RomanChygryn <chigrin.ra@gmail.com>
2018-07-06 13:42:10 +0200

    wtf

--- ico/js/common.js
+++ ico/js/common.js
@@ -1,35 +1,40 @@
-$('.slick-carousel-basic').slick({
 
-    slidesToShow: 4,
-    slidesToScroll: 0,
-    speed : 600,
-    ease : 'easeIn',
-    responsive: [
-        {
-            breakpoint: 1024,
-            settings: {
-                slidesToShow: 3,
-                slidesToScroll: 1,
-                infinite: true,
-                dots: false
-            }
-        },
-        {
-            breakpoint: 768,
-            settings: {
-                slidesToShow: 2,
-                slidesToScroll: 1
-            }
-        },
-        {
-            breakpoint: 480,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
-            }
-        }
-        // You can unslick at a given breakpoint now by adding:
-        // settings: "unslick"
-        // instead of a settings object
-    ]
+$( document ).ready(function() {
+  $('.slick-carousel-basic').slick({
+
+      slidesToShow: 4,
+      slidesToScroll: 0,
+      speed : 600,
+      ease : 'easeIn',
+      responsive: [
+          {
+              breakpoint: 1024,
+              settings: {
+                  slidesToShow: 3,
+                  slidesToScroll: 1,
+                  infinite: true,
+                  dots: false
+              }
+          },
+          {
+              breakpoint: 768,
+              settings: {
+                  slidesToShow: 2,
+                  slidesToScroll: 1
+              }
+          },
+          {
+              breakpoint: 480,
+              settings: {
+                  slidesToShow: 1,
+                  slidesToScroll: 1
+              }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+      ]
+  });
+
+  $('.header__bg__content').play();
 });
